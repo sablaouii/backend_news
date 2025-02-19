9e7e7f801d1e30478d4a6d54505690e885fe0a03
sablaouii <sablaouimariem@gmail.com>
2025-02-19 12:25:30 +0100

    Cruduser

---
+++ controllers/userController.js
@@ -0,0 +1,156 @@
+const userModel = require('../models/userSchema');
+//add user
+module.exports.addUserEmployeur = async (req,res) => {
+    try {
+        const {username , email , password , age} = req.body;
+        const roleEmployeur ='employeur'
+        const user = await userModel.create({
+            username,email,password,role:roleEmployeur,age
+        })
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+module.exports.addUserEmployeurWithImg = async (req,res) => {
+    try {
+        const {username , email , password } = req.body;
+        const roleEmployeur = 'employeur'
+        const {filename} = req.file
+
+        const user = await userModel.create({
+            username,email ,password,role : roleEmployeur , image_user : filename
+        })
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+// ajout d'admin
+module.exports.addUserAdmin= async (req,res) => {
+    try {
+        const {username , email , password}  = req.body;
+        const roleAdmin = 'admin'
+        const user = await userModel.create({
+            username,email ,password,role :roleAdmin
+        })
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+// donner tous les users 
+module.exports.getAllUsers= async (req,res) => {
+    try {
+        const userListe = await userModel.find() 
+        res.status(200).json({userListe});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+//recherche by id 
+module.exports.getUserById= async (req,res) => {
+    try {
+        //const id = req.params.id
+        const {id}=req.params
+       // console.log(req.params)
+        const user = await userModel.findById(id) 
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+//supprimer by id 
+module.exports.deleteUserById= async (req,res) => {
+    try {
+        const {id} = req.params
+//si l'utulisateur existe ou nn
+        const user = await userModel.findById(id);
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        await userModel.findByIdAndDelete(id)
+
+        res.status(200).json("deleted");
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+// modifier 
+module.exports.updateUserById = async (req, res) => {
+    try {
+        const {id} = req.params
+        const {email , username} = req.body;
+    
+        await userModel.findByIdAndUpdate(id,{$set : {email , username }})
+        const updated = await userModel.findById(id)
+    
+        res.status(200).json({updated})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    }
+    module.exports.searchUserByUsername = async (req, res) => {
+        try {
+    
+            const { username } = req.query
+            if(!username){
+                throw new Error("Veuillez fournir un nom pour la recherche.");
+            }
+    
+            const userListe = await userModel.find({
+                username: {$regex: username , $options: "i"}
+            })
+    
+            if (!userListe) {
+                throw new Error("User not found");
+              }
+              const count = userListe.length
+            res.status(200).json({userListe,count})
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
+        }
+        //trii
+        module.exports.getAllUsersSortByAge= async (req,res) => {
+            try {
+                //men sghyr l kbir
+                const userListe = await userModel.find().sort({age : 1}).limit(2)//l 5 lwelaa hethika pts limit
+                //men kbir l sghyr
+                //const userListe = await userModel.find().sort({age : -1})
+                res.status(200).json({userListe});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+        module.exports.getAllUsersAgeBetMaxAgeMinAge= async (req,res) => {
+            try {
+                const MaxAge = req.query.MaxAge
+                const MinAge = req.query.MinAge
+                const userListe = await userModel.find({age:{$gt: MinAge,$lt:MaxAge}}).sort({age:1})
+              
+                res.status(200).json({userListe});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+        module.exports.getAllEmployeur= async (req,res) => {
+            try {
+                const userListe = await userModel.find({role : "employeur"})
+              
+                res.status(200).json({userListe});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+        module.exports.getAllAdmin= async (req,res) => {
+            try {
+                const userListe = await userModel.find({role : "admin"})
+                res.status(200).json({userListe});
+            } catch (error) {
+                res.status(500).json({message: error.message});
+            }
+        }
+        
---
+++ routes/usersRouter.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const userController = require('../controllers/userController');
+const upload = require('../midlewares/uploadFile');
+
+/* GET users listing. */
+
+router.post('/addUserEmployeur',userController.addUserEmployeur);
+router.post('/addUserAdmin',userController.addUserAdmin); 
+router.get('/getAllUsers',userController.getAllUsers); 
+router.get('/getUserById/:id',userController.getUserById); 
+router.get('/searchUserByUsername',userController.searchUserByUsername); 
+router.get('/searchUserByUsername',userController.searchUserByUsername);
+router.get('/getAllUsersSortByAge',userController.getAllUsersSortByAge); 
+router.get('/getAllEmployeur',userController.getAllEmployeur); 
+router.get('/getAllAdmin',userController.getAllAdmin); 
+router.get('/getAllUsersAgeBetMaxAgeMinAge',userController.getAllUsersAgeBetMaxAgeMinAge); 
+//ajouter donc =post
+router.post('/addUserEmployeurWithImg/:id',upload.single("image_user"),userController.addUserEmployeurWithImg); 
+ 
+
+
+//router.get('/getAllEmployeur',userController.getAllEmployeur); //
+module.exports = router;
